42ed6062f53d522658d922d5819ddd4c724ce5a1
HalfS0ur <riveras.kendall99@gmail.com>
2024-07-25 19:02:08 -0600

    Added half_adder module

---
+++ Half_Adder/half_adder.sv
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module half_adder (
+    input  logic a_i,
+    input  logic b_i,
+    output logic sum_o,
+    output logic carry_o
+);
+
+always_comb begin
+    sum_o = a_i ^ b_i;
+    carry_o = a_i & b_i;
+end
+    
+endmodule
